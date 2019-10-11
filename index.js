14b6c6313f4ead89417454bb6912440d18ad6511
pivey <peteriveyhansen@gmail.com>
2019-10-11 10:27:31 +0200

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const PORT = 3001;
+
+const app = express();
+app.use(cors());
+
+app.get('/welcome', (req, res) => {
+  console.log('hello');
+  res.status(200).send('Tillsammans	gör	vi	det	lättare	för	ditt	företag	att	växa');
+});
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
